7b706e6b0dfd95a730e44a4756e12fdbfe103ae3
Conrad Lundberg <conrizzo@gmail.com>
2023-12-03 22:23:37 +0100

    update config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://conrizzo.github.io',    
-  base: '/',
+  base: '/ConradsBlog',
  // base: '/AstroWebsite/',
   integrations: [mdx(), sitemap(), vue()],    
 });
