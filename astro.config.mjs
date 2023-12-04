3a01938cc7a29c350b8c7b3063f7770bf3bb60ee
Conrad Lundberg <conrizzo@gmail.com>
2023-12-04 12:33:32 +0100

    Delete the whole project and restart

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+
 import sitemap from '@astrojs/sitemap';
 
-import vue from "@astrojs/vue";
-
-//  https://astro.build/config
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://conrizzo.github.io/ConradsBlog',      
-  //base: '/ConradsBlog',
-  base: '/',
-  integrations: [mdx(), sitemap(), vue()],    
+	site: 'https://conrizzo.github.io',
+	integrations: [mdx(), sitemap()],
 });
-
-
-
-
-
--- lasercat.py
+++
@@ -1,6 +0,0 @@
-laser_cat = ["pew","pew1","pew2"]
-
-for i in laser_cat:
-    print(i)
-
-    # Path: laser_cat.py
