99e164cd527a5b4c8059ec8b859c4caa96c76b9e
Conrad Lundberg <conrizzo@gmail.com>
2023-12-06 00:20:08 +0100

    change subdomain for website

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://conrizzo.github.io',
+	site: 'https://blog.conradswebsite.com',
 	base: '/ConradsBlog',
 	integrations: [mdx(), sitemap(), vue()],
 });
